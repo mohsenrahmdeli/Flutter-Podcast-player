35c9cbd86e72726df0d84fa8b987fbf605046177
MohsenRahmdeli <mohsen.rahmdeli@gmail.com>
2024-12-12 19:52:48 +0400

    inintialize repository

---
+++ lib/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Podcast Player'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() {
+  runApp(FlutterPodcastAudioApp());
+}
+
+/// This is just an app made for fun to do test with audio
+/// This Architecture is used for small-medium sized app,
+/// if you want to make something bigger you can use getXpattern with getX
+///
+class FlutterPodcastAudioApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Podcast App Demo',
+      home: HomeScreen(),
+    );
+  }
+}
